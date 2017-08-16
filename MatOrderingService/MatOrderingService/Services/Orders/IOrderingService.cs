f83782ef516372367805b6a6d9ae81bb5112a3d0
Pavel Shylan <pavel.shylan@materialise.kiev.ua>
2017-08-16 14:43:37 +0300

    Template for Presentation & IOrderingService

---
+++ MatOrderingService/MatOrderingService/Services/Orders/IOrderingService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MatOrderingService.Services.Orders
+{
+    public interface IOrderingService
+    {
+        Task<OrderInfo[]> GetAll();
+
+        Task<OrderInfo> Get(int id);
+
+        Task<OrderInfo> Create(NewOrder order);
+
+        Task<OrderInfo> Update(int id, EditOrder value);
+
+        Task<bool> Delete(int id);
+    }
+
+    public class EditOrder
+    {
+        public string OrderDetails { get; set; }
+    }
+
+    public class NewOrder
+    {
+        public string OrderDetails { get; set; }
+
+        public string CreatorId { get; set; }
+    }
+
+    public class OrderInfo
+    {
+        public int Id { get; set; }
+
+        public string OrderDetails { get; set; }
+
+        public String Status { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string CreatorId { get; set; }
+    }
+}
